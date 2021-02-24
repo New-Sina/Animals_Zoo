be1d3f4ad89d4e69a4b4a6fd4ec9d5e6764166e2
canghaiyishengxiao2021 <79012961+canghaiyishengxiao2021@users.noreply.github.com>
2021-02-24 15:56:23 +0800

    Update Ponysitters_Club_Season.js

--- Ponysitters_Club_Season.js
+++ Ponysitters_Club_Season.js
@@ -112,8 +112,8 @@ async function executeOneByOne() {
           
   } else {
          
-      // console.log(hideme(rcontent));
-console.log(rcontent);
+       console.log(hideme(rcontent));
+//console.log(rcontent);
      }
     }
 }
